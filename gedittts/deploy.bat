36b089fb79335528cdd729ab888257bf9ce5fd4f
Robert <rwallhead@gmail.com>
2012-12-26 08:21:24 +1100

    Working plug-in (entirely from https://live.gnome.org/Gedit/PythonPluginHowToOld)

--- gedittts/deploy.bat
+++ gedittts/deploy.bat
@@ -1,6 +1,9 @@
 :: This is working on Win7 64bit, try at own risk
 :: Oh, and because of all sorts of reasons, this must be ran as administrator...
 
+@echo off
+
+taskkill /F /IM gedit.exe
 copy gedittts.gedit-plugin "C:\Program Files (x86)\gedit\lib\gedit-2\plugins"
 copy gedittts.py "C:\Program Files (x86)\gedit\lib\gedit-2\plugins"
-
+start "gedit" "c:\Program Files (x86)\gedit\bin\gedit.exe" 2> %tmp%\output.txt
--- gedittts/gedittts.py
+++ gedittts/gedittts.py
@@ -1,11 +1,92 @@
+from gettext import gettext as _
+
+import gtk
 import gedit
 
-class ExamplePyPlugin(gedit.Plugin):
+# Menu item example, insert a new item in the Tools menu
+ui_str = """<ui>
+  <menubar name="MenuBar">
+    <menu name="ToolsMenu" action="Tools">
+      <placeholder name="ToolsOps_2">
+        <menuitem name="GeditTTS" action="GeditTTS"/>
+      </placeholder>
+    </menu>
+  </menubar>
+</ui>
+"""
+
+class GeditTTSWindowHelper:
+    def __init__(self, plugin, window):
+        print "Plugin created for", window
+        
+        self._window = window
+        self._plugin = plugin
+
+        # Insert menu items
+        self._insert_menu()
+
+    def deactivate(self):
+        print "Plugin stopped for", self._window
+        
+        # Remove any installed menu items
+        self._remove_menu()
+        
+        self._window = None
+        self._plugin = None
+        self._action_group = None
+
+    def _insert_menu(self):
+        # Get the GtkUIManager
+        manager = self._window.get_ui_manager()
+
+        # Create a new action group
+        self._action_group = gtk.ActionGroup("GeditTTSPluginActions")
+        self._action_group.add_actions([("GeditTTS", None, _("Clear document"),
+                                         None, _("Clear the document"),
+                                         self.on_clear_document_activate)])
+
+        # Insert the action group
+        manager.insert_action_group(self._action_group, -1)
+
+        # Merge the UI
+        self._ui_id = manager.add_ui_from_string(ui_str)
+
+    def _remove_menu(self):
+        # Get the GtkUIManager
+        manager = self._window.get_ui_manager()
+
+        # Remove the ui
+        manager.remove_ui(self._ui_id)
+
+        # Remove the action group
+        manager.remove_action_group(self._action_group)
+
+        # Make sure the manager updates
+        manager.ensure_update()
+
+    def update_ui(self):
+        self._action_group.set_sensitive(self._window.get_active_document() != None)
+
+    # Menu activate handlers
+    def on_clear_document_activate(self, action):
+        doc = self._window.get_active_document()
+        if not doc:
+            return
+
+        doc.set_text('')
+
+
+class GeditTTS(gedit.Plugin):
+    def __init__(self):
+        gedit.Plugin.__init__(self)
+        self._instances = {}
+
     def activate(self, window):
-        pass
+        self._instances[window] = GeditTTSWindowHelper(self, window)
 
     def deactivate(self, window):
-        pass
+        self._instances[window].deactivate()
+        del self._instances[window]
 
     def update_ui(self, window):
-        pass
+        self._instances[window].update_ui()
