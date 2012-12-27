45420363f21716a403a19b3170ad0a6ddee64b56
Robert <rwallhead@gmail.com>
2012-12-28 09:10:51 +1100

    Fixed up the testrunner to restart properly. This was needed as it wasn't reloading the .py files between runs, meaning I couldn't just leave it open while coding to pass some tests.

--- runtests.py
+++ runtests.py
@@ -7,17 +7,21 @@ import sys
 sys.path.append(".\src")
 opts = doctest.REPORT_ONLY_FIRST_FAILURE|doctest.ELLIPSIS|doctest.NORMALIZE_WHITESPACE
 
-while True:
-    files = glob.glob('tests\*.rst')
-    failure = False
-    for f in files:
-        failed, tested = doctest.testfile(f, optionflags=opts)
-        if failed > 0:
-            failure = True
 
-    if not failure:
-        print "All tests passed!"
+files = glob.glob('tests\*.rst')
+failure = False
+for f in files:
+    failed, tested = doctest.testfile(f, optionflags=opts)
+    if failed > 0:
+        failure = True
 
-    print "\nPress enter to re-run...",
-    raw_input()
-    os.system(['clear','cls'][os.name == 'nt'])
+if not failure:
+    print "All tests passed!"
+
+print "\nPress enter to re-run...",
+raw_input()
+os.system(['clear','cls'][os.name == 'nt'])
+
+# restart myself
+python = sys.executable
+os.execl(python, python, * sys.argv)
